75c0df1b2b7bdfc9b0407e80e4c93096604421e0
Justin Bronn <justin.bronn@maxar.com>
2022-07-21 13:40:32 -0600

    Add cluster CA data to outputs.

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
+output "cluster_certificate_authority_data" {
+  description = "Base64 encoded certificate data required to communicate with the cluster"
+  value       = module.eks.cluster_certificate_authority_data
+}
+
 output "cluster_endpoint" {
   description = "Endpoint for your Kubernetes API server"
   value       = module.eks.cluster_endpoint
