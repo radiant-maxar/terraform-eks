6d382a8b11244505e525900dfe45ee8055b4fa5a
Justin Bronn <justin.bronn@maxar.com>
2023-10-17 12:18:02 -0600

    Upgrade AWS EBS/EFS CSI and load balancer controller versions to latest.

--- variables.tf
+++ variables.tf
@@ -95,13 +95,13 @@ variable "default_max_size" {
 }
 
 variable "ebs_csi_driver_version" {
-  default     = "2.21.0"
+  default     = "2.24.0"
   description = "Version of the EFS CSI storage driver to install."
   type        = string
 }
 
 variable "efs_csi_driver_version" {
-  default     = "2.4.7"
+  default     = "2.5.0"
   description = "Version of the EFS CSI storage driver to install."
   type        = string
 }
@@ -130,7 +130,7 @@ variable "iam_role_attach_cni_policy" {
 }
 
 variable "lb_controller_version" {
-  default     = "1.5.5"
+  default     = "1.6.1"
   description = "Version of the AWS Load Balancer Controller chart to install."
   type        = string
 }
