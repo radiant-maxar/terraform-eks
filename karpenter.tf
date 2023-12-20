fa093ee663858695f86a5a21cab45ba58e640494
Justin Bronn <justin.bronn@maxar.com>
2023-12-19 17:22:35 -0700

    Set `enable_karpenter_instance_profile_creation=true` by default.

--- karpenter.tf
+++ karpenter.tf
@@ -13,20 +13,20 @@ module "karpenter" {
   source  = "terraform-aws-modules/eks/aws//modules/karpenter"
   version = "19.21.0"
 
-  cluster_name           = var.cluster_name
-  irsa_oidc_provider_arn = module.eks.oidc_provider_arn
-  tags                   = var.tags
+  cluster_name                               = var.cluster_name
+  enable_karpenter_instance_profile_creation = true
+  irsa_oidc_provider_arn                     = module.eks.oidc_provider_arn
+  tags                                       = var.tags
 }
 
 resource "helm_release" "karpenter_crd" {
   count            = var.karpenter ? 1 : 0
-  namespace        = "karpenter"
   create_namespace = true
-
-  name       = "karpenter-crd"
-  repository = "oci://public.ecr.aws/karpenter"
-  chart      = "karpenter-crd"
-  version    = "v${var.karpenter_version}"
+  name             = "karpenter-crd"
+  namespace        = "karpenter"
+  repository       = "oci://public.ecr.aws/karpenter"
+  chart            = "karpenter-crd"
+  version          = "v${var.karpenter_version}"
 
   # XXX: Unfortunately, AWS ECR credentials leads to resource churn
   #      as the password will change.
@@ -40,11 +40,9 @@ resource "helm_release" "karpenter_crd" {
 }
 
 resource "helm_release" "karpenter" {
-  count            = var.karpenter ? 1 : 0
-  namespace        = "karpenter"
-  create_namespace = true
-
+  count      = var.karpenter ? 1 : 0
   name       = "karpenter"
+  namespace  = "karpenter"
   repository = "oci://public.ecr.aws/karpenter"
   chart      = "karpenter"
   version    = "v${var.karpenter_version}"
