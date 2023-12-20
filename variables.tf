e99cc19869cf141ffa20a2ed43076a9e230be890
Justin Bronn <justin.bronn@maxar.com>
2023-12-20 11:41:43 -0700

    Attempt Karpenter 0.32.4 instead.

--- variables.tf
+++ variables.tf
@@ -217,7 +217,7 @@ variable "karpenter_values" {
 variable "karpenter_version" {
   description = "Version of Karpenter Helm chart to install on the EKS cluster."
   type        = string
-  default     = "0.33.1"
+  default     = "0.32.4"
 }
 
 variable "kms_manage" {
