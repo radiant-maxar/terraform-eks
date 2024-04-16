f4182deb45d9ca6fd91c0845ef2d2d8578a62e10
Justin Bronn <justin.bronn@maxar.com>
2024-04-16 14:06:44 -0600

    Upgrae Karpenter module to at least 20.8.5 too.

--- karpenter.tf
+++ karpenter.tf
@@ -1,7 +1,7 @@
 module "karpenter" {
   count   = var.karpenter ? 1 : 0
   source  = "terraform-aws-modules/eks/aws//modules/karpenter"
-  version = "~> 20.8.4"
+  version = "~> 20.8.5"
 
   cluster_name                      = var.cluster_name
   enable_irsa                       = true
