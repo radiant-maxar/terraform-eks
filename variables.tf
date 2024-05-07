99209921b4c4e86416dad6bcc6ca1ae4e0ece168
Justin Bronn <justin.bronn@maxar.com>
2024-05-07 09:51:41 -0600

    Upgrade EFS CSI driver chart version to 3.0.3.

--- variables.tf
+++ variables.tf
@@ -305,7 +305,7 @@ variable "efs_csi_driver_values" {
 }
 
 variable "efs_csi_driver_version" {
-  default     = "2.5.7"
+  default     = "3.0.3"
   description = "Version of the EFS CSI storage driver to install."
   type        = string
 }
