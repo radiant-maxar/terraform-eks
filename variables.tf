73df109891c13ada4a8d97e4bbd76f2ae060b0a2
Justin Bronn <jbronn@gmail.com>
2025-02-05 12:24:21 -0700

    Upgrade NVIDIA GPU Operator version to 24.9.2.

--- variables.tf
+++ variables.tf
@@ -609,7 +609,7 @@ variable "nvidia_gpu_operator_values" {
 }
 
 variable "nvidia_gpu_operator_version" {
-  default     = "24.9.0"
+  default     = "24.9.2"
   description = "Version of the NVIDIA GPU Operator Helm chart to install."
   type        = string
 }
